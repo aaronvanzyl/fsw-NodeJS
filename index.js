76ee4d94690f0652d7ef95e2ff4c58f7253eb6da
aaronvanzyl <aaron@vanzyl.org>
2020-05-19 20:01:08 -0700

    Add callbacks to rectangle module

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 var rect = require('./rectangle')
 
-console.log(rect.area(4,5));
+function testRect(x, y) {
+    console.log("testing " + x + "," + y);
+    rect(x, y, (error, rectObj) => {
+        if(error) {
+            console.log(x + "," + y + " - ERROR: " + error);
+        }
+        else {
+            console.log(x + "," + y + " - success: a=" + rectObj.area() + " p=" + rectObj.perimeter());
+        }
+
+    });
+}
+
+testRect(1,5);
+testRect(2,5);
+testRect(-10,5);
+testRect(1,-20);
+testRect(2,2);
--- rectangle.js
+++ rectangle.js
@@ -1,1 +1,19 @@
-exports.area = (x,y) => (x*y);
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(
+            () => callback(new Error("error bad fail"), null)
+            , 2000);
+    }
+    else {
+        setTimeout(
+            () => callback(null,
+                {
+                    area: () => (x * y),
+                    perimeter: () => (2 * (x + y))
+                })
+            , 2000);
+    }
+
+}
+
+
